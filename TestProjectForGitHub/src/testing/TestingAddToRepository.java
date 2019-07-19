5be79742bdb6631f9247f73a8734f92bb645494f
Adeel <Adeel@DESKTOP-PLHC3JT>
2019-07-19 12:01:31 -0400

    Added notes in the class for how to use GitHub

--- TestProjectForGitHub/src/testing/TestingAddToRepository.java
+++ TestProjectForGitHub/src/testing/TestingAddToRepository.java
@@ -1,5 +1,25 @@
 package testing;
 
 public class TestingAddToRepository {
+	
+	/*//Steps on how to Add a project to the GitHub and how to update it and how to pull from it. 
+	Step 1 : Create GitHub account and SignIn
+
+	Step 2 : Start a Project = Create a repository
+
+	Step 3 : Start Eclipse
+
+	Step 4 : Goto Perspective - Git Repositories and click on Add Git Repo
+
+	Step 5 : Create a project in Eclipse
+
+	Step 6 : Do a right click on Project - Team - Share - Add to git repo
+
+	Step 7 : Commit and Push the project to the repo
+
+	Step 8 : Commit and Push every change to the repo
+	
+	*/
+
 
 }
